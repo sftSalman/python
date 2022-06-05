446bd214fa91c4275c3151f80ed0e4828ecf5351
salman <salmanfarshinsu@gmail.com>
2022-06-05 10:44:07 +0600

    counter substract and update

--- counter.py
+++ counter.py
@@ -1,23 +1,9 @@
 from collections import Counter
-
-counter = Counter(['a', 'a', 'b'])
-print(counter)
-# adding new counter value
-counter['d']=4
-print(counter)
-# deleting value
-
-del counter['d']
-print(counter)
-
-# elements which return only positive value
-
-counter['n']=-1
-print(counter)
-print(counter.elements())
-el= counter.elements()
-for i in el:
-    print(i)
-# most common
-mcn = counter.most_common(1)
-print(mcn)
+c = Counter('abaadefs')
+print(c)
+s = Counter('a')
+c.subtract(s)
+print(c)
+n = Counter('a')
+c.update(n)
+print(c)

6b46cb7a4e5d60baa24d0e78254f1131624a9afd
salman <salmanfarshinsu@gmail.com>
2022-06-05 11:01:01 +0600

    hekher rank problem about show price count: NOT compplete

--- shoe_sell_Dictionary.py
+++ shoe_sell_Dictionary.py
@@ -1,6 +1,12 @@
 from collections import Counter
 n = input()
-sn = map(int,input().split())
-pr = str(sn)
-print(pr)
-
+sn = Counter(map(int,input().split()))
+nn = input()
+nnn = input(n)
+earnings = 0
+for i in range(nnn):
+    size, price = map(int,input().split())
+    if size in sn and sn[size]>0:
+    sn[size] -= 1
+    earnings = earnings + price
+print(earnings)

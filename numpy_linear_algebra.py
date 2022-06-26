a746cd1d10e5b4c6b829f6d0c8b4be565750e891
salman <salmanfarshinsu@gmail.com>
2022-06-26 11:55:17 +0600

    numpy linear algebra practice

---
+++ numpy_linear_algebra.py
@@ -0,0 +1,11 @@
+import numpy as np
+a = [1,2,3,4]
+b = [2,3,4,5]
+inn = np.inner(a,b)
+print(inn)
+outt = np.outer(a,b)
+print(outt)
+matmul = np.matmul(a,b)
+print(matmul)
+dot = np.dot(a,b)
+print(dot)

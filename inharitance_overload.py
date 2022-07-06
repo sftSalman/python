52a61def99a5e33d542c4c2977c7675028fef7af
salman <salmanfarshinsu@gmail.com>
2022-07-06 07:46:05 +0600

    inharitance with magic function

---
+++ inharitance_overload.py
@@ -0,0 +1,11 @@
+class Person():
+    def __init__(self,name,age, gender):
+        self.name  = name
+        self.age = age
+        self.gender = gender
+
+    def __str__(self):
+        print(self.name,self.age,self.gender)
+
+p = Person('Salman',23,'male')
+print(p.name,p.age,p.gender)

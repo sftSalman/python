c5c568da606ecdcffacab89a37df040918eecf0e
salman <salmanfarshinsu@gmail.com>
2022-07-07 11:43:09 +0600

    student class has inharited using super()

--- add_item_inSuper_class.py
+++ add_item_inSuper_class.py
@@ -7,15 +7,19 @@ class Person:
         print(self.first_name,self.last_name)
 
 class Student(Person):
+
     def __init__(self,fname,lname,age):
+        super().__init__(fname,lname)
         self.first_name = fname
         self.last_name = lname
         self.age = age
-    def printname(self):
+    def new_printname(self):
         print(self.first_name,self.last_name,self.age)
+
 
 
 x = Person('salman','farshi')
 x2 = Student('sami','salvi',34)
 x.printname()
 x2.printname()
+x2.new_printname()

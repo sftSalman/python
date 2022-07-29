3ea91a43da8e75ba34da0584d089acc600bd14b0
salmanFarshi <salmanfarshinsu@gmail.com>
2022-07-29 22:49:06 +0600

    emmurate

---
+++ try_except.py
@@ -0,0 +1,15 @@
+# try:
+#   f = open("demofile.txt")
+#   try:
+#     f.write("Lorum Ipsum")
+#   except:
+#     print("Something went wrong when writing to the file")
+#   finally:
+#     f.close()
+# except:
+#   print("Something went wrong when opening the file")
+#
+x = -1
+
+if x < 0:
+      raise Exception("Sorry, no numbers below zero")

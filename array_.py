4e8227591abc4d22c7db66f76c3c435ecbf63f7e
salmanFarshi <salmanfarshinsu@gmail.com>
2022-08-11 09:07:36 +0600

    python array

---
+++ array_.py
@@ -0,0 +1,11 @@
+from array import *
+
+array1 = array('i',[10,20,30,40,50])
+
+# for x in array1:
+#    print(x)
+
+print(array1[2:6])
+
+array1.insert(3,44)
+print(array1)
--- sorted_list.py
+++ sorted_list.py
@@ -1,12 +1,25 @@
-my_list = [4, 2, 3, -1, -2, 0, 1]
-
-for i in range(len(my_list)):
-    for j in range(i + 1, len(my_list)):
-
-        if my_list[i] > my_list[j]:
-            my_list[i], my_list[j] = my_list[j], my_list[i]
-
-print(my_list)
-
-my_list.insert(2,2)
-print(my_list)
+class Stack
+def_init_(self):
+self.items=[]
+def is_empty(self):
+        return self.items==[]
+    def push(self, data):
+        self.items.append(data)
+    def pop(self):
+        return self.items.pop()
+s= Stack()
+while True:
+    print(‘push<value>’)
+    print(‘pop’)
+    print(‘quit’)
+do= input(‘What would you like to do?’).split()
+operation= do[0].strip().lower()
+if operation== ‘push’:
+s.push(int(do[1]))
+elif operation== ‘pop’:
+    if s.is_empty():
+        print(‘Stack is empty’)
+    else:
+        print(‘Popped value:’, s.pop())
+elif operation==’quit’:
+break

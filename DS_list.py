b689541e7a02a87abd96e35a98030e4149f7a6c0
salmanFarshi <salmanfarshinsu@gmail.com>
2022-08-16 07:42:31 +0600

    Comment added from --grec

--- DS_list.py
+++ DS_list.py
@@ -1,3 +1,9 @@
+# Python Lists are just like the arrays, declared in other languages which is an ordered collection of data. It is very flexible as the items in a list do not need to be of the same type.
+#
+# The implementation of Python List is similar to Vectors in C++ or ArrayList in JAVA. The costly operation is inserting or deleting the element from the beginning of the List as all the elements are needed to be shifted. Insertion and deletion at the end of the list can also become costly in the case where the preallocated memory becomes full.
+#
+# We can create a list in python as shown below.
+
 l = [1,2,3,4]
 print(l[-1])
 
@@ -8,6 +14,10 @@ print(d.get(('name')))
 
 # tuple
 
+#
+# Python dictionary is like hash tables in any other language with the time complexity of O(1). It is an unordered collection of data values, used to store data values like a map, which, unlike other Data Types that hold only a single value as an element, Dictionary holds the key:value pair. Key-value is provided in the dictionary to make it more optimized.
+#
+# Indexing of Python Dictionary is done with the help of keys. These are of any hashable type i.e. an object whose can never change like strings, numbers, tuples, etc. We can create a dictionary by using curly braces ({}) or dictionary comprehension.
+
 t = ('salman',1 ,3)
 print(t[1])
-t.
